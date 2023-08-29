8ff5d3b47ecf3b0d2f447ba63eb53c7fffb0d9e4
root <RobinLeeHenry101@gmail.com>
2023-08-29 07:44:41 -0700

    finished everything except the last two advanced questions

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+"""define class Square"""
+
+class Square:
+    """Square"""
+    pass
---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""define a class Square"""
+
+
+class Square:
+    """represent a square"""
+
+    def __init__(self, size):
+        """initialize a new Square
+
+        argument:
+            size (int): The size of the new square
+        """
+        self.__size = size
