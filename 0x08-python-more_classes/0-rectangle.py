1289cfa57e79222a7aaafc3d5ff068d93ec77e07
root <RobinLeeHenry101@gmail.com>
2023-09-04 03:47:02 -0700

    pushing first two asnwers

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+
+class Rectangle:
+    """defines an empty class called rectangle"""
+    pass
---
+++ 0x08-python-more_classes/2-rectangle.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+
+class Rectangle:
+    """initializes rectangle"""
+    def __init__(self, width=0, height=0):
+        
