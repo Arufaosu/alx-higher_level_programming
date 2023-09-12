c3c0ab0a6d7c6c649edf628358b7c15407453f04
root <RobinLeeHenry101@gmail.com>
2023-09-12 10:13:07 -0700

    fixed task 7 and advanced

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,9 +1,16 @@
 #!/usr/bin/python3
 """a JSON file-reading function"""
-import json
+import sys
 
 
-def load_from_json_file(filename):
-    """creates a python object"""
-    with open(filename) as f:
-        return json.load(f)
+if __name__ == "__main__":
+    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+    load_from_json_file = \
+        __import__('6-load_from_json_file').load_from_json_file
+
+    try:
+        items = load_from_json_file("add_item.json")
+    except FileNotFoundError:
+        items = []
+    items.extend(sys.argv[1:])
+    save_to_json_file(items, "add_item.json")
