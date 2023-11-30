8eccc8538ced3214d2aa3a44582cff457389b26b
root <RobinLeeHenry101@gmail.com>
2023-11-30 09:18:45 -0800

    made task 0 in 3 lines

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
 # takes in a URL, sends a request to that UR
-
-if [ -z "$1" ]; then
-    echo "Usage: $0 <URL>"
-    exit 1
-fi
-
-size=$(curl -s -H "Accept-Encoding: gzip, deflate" "$1" | wc -c)
-echo "$size"
+[ -z "$1" ] && { echo "Usage: $0 <URL>"; exit 1; } || size=$(curl -s -H "Accept-Encoding: gzip, deflate" "$1" | wc -c); echo "$size"
