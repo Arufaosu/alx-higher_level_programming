ef0d4b6940534b790d679038d1895124b07e9c35
root <RobinLeeHenry101@gmail.com>
2023-12-01 04:48:07 -0800

    fixed code for task 8

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -3,22 +3,21 @@ import requests
 import sys
 
 """ sends a POST request to http://0.0.0.0:5000/search_user """
-if len(sys.argv) == 1:
-    q = ""
-else:
-    q = sys.argv[1]
-
-url = "http://0.0.0.0:5000/search_user"
-
-response = requests.post(url, data={'q': q})
-
-try:
-    data = response.json()
-
-    if data:
-        print("[{}] {}".format(data.get('id'), data.get('name')))
+if __name__ == "__main__":
+    if len(sys.argv) == 1:
+        q = ""
     else:
-        print("No result")
+        q = sys.argv[1]
 
-except ValueError:
-    print("Not a valid JSON")
+    url = "http://0.0.0.0:5000/search_user"
+    payload = {"q": q}
+    response = requests.post(url, data=payload)
+
+    try:
+        json_response = response.json()
+        if json_response:
+            print("[{}] {}".format(json_response['id'], json_response['name']))
+        else:
+            print("No result")
+    except ValueError:
+        print("Not a valid JSON")
